d820ba4619af65863b547ecccaf81da484b639b6
Matheus Felipe Rodrigues <matheusfelipercontapessoal@gmail.com>
2024-09-22 23:12:34 -0300

    - Implementada nova função para baixar vídeos e áudios do YouTube. - Adicionado suporte para formatos MP3 e MP4. - Corrigida manipulação de caminhos para arquivos baixados, garantindo renomeação adequada. - Melhorada a entrada do usuário para seleção de formato.

--- downloader.py
+++ downloader.py
@@ -5,24 +5,34 @@ import argparse
 def create_output_directory(output_path):
     os.makedirs(output_path, exist_ok=True)
 
-def download_video(url, output_path='video', format='mp4'):
+def download_video(url, output_path='download', format='mp4'):
     create_output_directory(output_path)
 
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegVideoConvertor',
-            'preferedformat': format,
-        }],
-        'ffmpeg_location': './bin/',
-        'progress_hooks': [hook],
-    }
+    if format == 'mp3':
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+            'ffmpeg_location': './bin/',
+        }
+    else:
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',
+            'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+            'ffmpeg_location': './bin/',
+        }
     
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url)
             ydl.download([url])
-        print(f"Vídeo baixado e convertido com sucesso para {output_path}")
+        
+        # Se for MP3, renomeia a extensão
+        if format == 'mp3':
+            original_filename = ydl.prepare_filename(info_dict)
+            new_filename = os.path.splitext(original_filename)[0] + '.mp3'
+            os.rename(original_filename, new_filename)
+
+        print(f"Arquivo baixado com sucesso para {output_path}")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
@@ -33,7 +43,6 @@ def hook(d):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Baixar vídeos do YouTube.')
     parser.add_argument('--output', type=str, default='video', help='Diretório de saída')
-    parser.add_argument('--format', type=str, default='mp4', help='Formato de saída do vídeo')
 
     args = parser.parse_args()
 
@@ -42,4 +51,11 @@ if __name__ == "__main__":
         if video_url.lower() == 'sair':
             print("Encerrando o programa.")
             break
-        download_video(video_url, args.output, args.format)
+        
+        # Escolha do formato
+        format = input("Escolha o formato de saída (mp4 ou mp3): ").strip().lower()
+        while format not in ['mp4', 'mp3']:
+            print("Formato inválido! Escolha 'mp4' ou 'mp3'.")
+            format = input("Escolha o formato de saída (mp4 ou mp3): ").strip().lower()
+
+        download_video(video_url, args.output, format)
